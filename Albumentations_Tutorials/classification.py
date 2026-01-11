e47e21d8a6e426279578a57a00465496af8b9dc6
SalmaLilad <140752716+SalmaLilad@users.noreply.github.com>
2026-01-10 20:52:45 -0600

    Update classification.py

--- Albumentations_Tutorials/classification.py
+++ Albumentations_Tutorials/classification.py
@@ -1,1 +1,77 @@
 
+
+import cv2
+import albumentations as A
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+
+def visualize(image):
+    plt.figure(figsize=(10, 10))
+    plt.axis("off")
+    plt.imshow(image)
+    plt.show()
+
+def visualize_bbox(img, bbox, class_name, color=(255, 0, 0), thickness=2):
+    """Visualizes a single bounding box on the image"""
+    x_min, y_min, x_max, y_max = bbox
+    x_min, x_max, y_min, y_max = int(x_min), int(x_max), int(y_min), int(y_max)
+    
+    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
+    
+    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
+    cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), color, -1)
+    cv2.putText(
+        img,
+        text=class_name,
+        org=(x_min, y_min - int(0.3 * text_height)),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=0.35, 
+        color=(255, 255, 255), 
+        lineType=cv2.LINE_AA,
+    )
+    return img
+
+def plot_examples(images, bboxes=None):
+    fig = plt.figure(figsize=(15, 15))
+    columns = 4
+    rows = 5
+    
+    for i in range(1, len(images)):
+        if bboxes is not None:
+            img = visualize_bbox(images[i - 1], bboxes[i - 1], class_name="Flower")
+        else:
+            img = images[i - 1]
+        fig.add_subplot(rows, columns, i)
+        plt.imshow(img)
+    plt.show()
+
+
+image = Image.open("Flower_Imgs/Flower - Rose.jpg")
+
+transform = A.Compose(
+    [
+        A.Resize(width=1920, height=1080),
+        A.RandomCrop(width=1280, height=780),
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(p=0.35),
+        A.Rotate(limit=40, p=0.9, border_mode=cv2.BORDER_CONSTANT),
+        A.VerticalFlip(p=0.1),
+        A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
+        #A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)), #Adjusts pixel values for consistency -> good for better neural network training
+        A.OneOf([
+            A.Blur(blur_limit=3, p=0.5),
+            A.ColorJitter(p=0.5)
+        ], p=1.0)
+    ]
+)
+
+images_list = [image]
+image = np.array(image)
+
+for i in range(15):
+    augmentations = transform(image=image)
+    augmented_img = augmentations["image"]
+    images_list.append(augmented_img)
+
+plot_examples(images_list)
